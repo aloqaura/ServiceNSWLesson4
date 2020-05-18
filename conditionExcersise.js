40bb779e6fe107c660fe362a3d6990cb8f5a81d8
alex.burgess3 <alex.burgess3@tafensw.edu.au>
2020-05-18 16:40:59 +1000

    Disregard update

--- conditionExcersise.js
+++ conditionExcersise.js
@@ -12,8 +12,7 @@
 
 /* 2 Create a function that takes 1 parameter that is an object with 2 properties, name and age. Return true 
 if the person's age is greater than 30 or their name contains the letter x (case insensitive). 
-Otherwise return false.
-*/
+Otherwise return false. */
 
 
 let person1 = {
